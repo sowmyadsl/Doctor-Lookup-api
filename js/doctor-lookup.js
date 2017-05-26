7f6f79b716dc411707a7cedc4828ec629cf5adaf
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-26 10:52:27 -0700

    Update the interface js file to show the search results on the page

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -6,12 +6,15 @@ function Doctor(medicalIssue) {
 
 Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
+  .then(function(result) {
+    $('.showDoctors').text(result.data.forEach(function(data) {
+      $('.showDoctors').append("<li>" + data.profile.first_name + " " + data.profile.middle_name + " " + data.profile.last_name + " " + data.profile.title + " " + data.profile.bio + "</li>");
       console.log(result);
-    })
-   .fail(function(error){
+    }));
+  })
+    .fail(function(error){
       console.log("fail");
     });
-};
+  };
 
-exports.doctorModule = Doctor;
+  exports.doctorModule = Doctor;
